43e99dfdc092aacc9e56c884851e6332e7d44e42
Peter <Posoo617@gmail.com>
2020-07-29 13:04:33 +0300

    creating components are organizing files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-import React from "react"
+import React, { Component } from "react"
+
 import logo from "./logo.svg"
 import "./App.css"
 
-function App() {
-	return <h1> Ecommerce </h1>
+import NavBar from "./components/NavBar"
+import ProductList from "./components/ProductList"
+import Details from "./components/Details"
+import Cart from "./components/Cart"
+import Default from "./components/Default"
+
+class App extends Component {
+	render() {
+		return (
+			<div>
+				app
+				<NavBar />
+				<ProductList />
+				<Details />
+				<Cart />
+				<Default />
+			</div>
+		)
+	}
 }
 
 export default App
---
+++ src/components/Default.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react"
+
+class Default extends Component {
+	render() {
+		return <div>default page</div>
+	}
+}
+
+export default Default
